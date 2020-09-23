fb021f25a9ab1c66ecbdb27a87fedfdb6ac88368
Ryosuke <ryosuke.san.hana@gmail.com>
2020-09-23 12:13:50 -0700

    :wrench: Wrap Storybook in theme

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,9 @@
-import { configure } from "@storybook/react";
+import { configure, addDecorator } from "@storybook/react";
+import { ThemeProvider } from "@shopify/restyle";
+import theme from "../themes/default";
+
+// Wrap all stories in Theme Provider
+addDecorator((story) => <ThemeProvider theme={theme}>{story()}</ThemeProvider>);
 
 // automatically import all files ending in *.stories.js
 configure(require.context("../stories", true, /\.stories\.js$/), module);
