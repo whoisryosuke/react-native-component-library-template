3f287a82aa381e143198b929b96260578bac542f
Ryosuke <ryosuke.san.hana@gmail.com>
2020-09-23 12:13:41 -0700

    :fire: Remove styled components refs

--- stories/1-Button.stories.js
+++ stories/1-Button.stories.js
@@ -2,16 +2,6 @@ import React from "react";
 import { action } from "@storybook/addon-actions";
 import { storiesOf } from "@storybook/react-native";
 import { Button } from "react-native";
-import styled from "styled-components/native";
-
-const StyledButton = styled.TouchableOpacity`
-  background-color: red;
-  padding: 3px;
-`;
-
-const StyledText = styled.Text`
-  color: white;
-`;
 
 export default {
   title: "Button",
@@ -20,17 +10,9 @@ export default {
 export const text = () => (
   <Button title="Hello Button" onPress={action("clicked")} />
 );
-export const styledComponent = () => (
-  <StyledButton onPress={action("clicked")}>
-    <StyledText>Hello Button</StyledText>
-  </StyledButton>
-);
 export const red = () => (
   <Button title="Hello Button" color="red" onPress={action("clicked")} />
 );
 
 // On-Device Register
-storiesOf("Button", module)
-  .add("Text", text)
-  .add("Styled Components", styledComponent)
-  .add("Red", red);
+storiesOf("Button", module).add("Text", text).add("Red", red);
