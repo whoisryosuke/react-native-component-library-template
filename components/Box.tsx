f32ce613057074e33c2afb847e70022891372bdb
Ryosuke <ryosuke.san.hana@gmail.com>
2020-09-23 12:14:21 -0700

    :zap: basic restyle components

---
+++ components/Box.tsx
@@ -0,0 +1,6 @@
+import { createBox } from "@shopify/restyle";
+import { Theme } from "../themes/default";
+
+const Box = createBox<Theme>();
+
+export default Box;
---
+++ components/Text.tsx
@@ -0,0 +1,6 @@
+import { createText } from "@shopify/restyle";
+import { Theme } from "../themes/default";
+
+const Text = createText<Theme>();
+
+export default Text;
